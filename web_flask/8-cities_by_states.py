f4fbfd7ef50f649ace4f6b9de3721a8a91c5b156
GideonBature <infoaboutgideon@gmail.com>
2023-12-30 04:02:28 +0100

    fix code

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/cities_by_states')
+def state_list():
+    """display a HTML page: inside the tag <BODY>
+    <H1> tag: 'States'
+    <UL> tag: 'List of all State objects present in DBStorage
+    <LI> tag: description of one State
+    """
+    states = storage.all(State).values()
+
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(exceptions):
+    """Remove the current SQLAlchemy Session
+    after each request
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
